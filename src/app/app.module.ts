1d7f33033bc75e0a3a82e050404176622ff9855f
johannes <johannes.hoellwerth@protonmail.com>
2022-06-02 18:09:38 +0200

    Added http.service.ts;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from "@angular/common/http";
+import { HeaderComponent } from './components/header/header.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
