29525c41fab9e7c70b277fa7fa17821f570a57f0
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-06-08 22:16:48 +0200

    Added About Component Added API Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HttpClientModule } from "@angular/common/http";
 import { HeaderComponent } from './components/header/header.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.component';
+import { AboutComponent } from './components/about/about.component';
+import { LoginComponent } from './components/login/login.component';
+import { ApiComponent } from './components/api/api.component';
 
 @NgModule({
   declarations: [
@@ -13,6 +16,9 @@ import { ScrollToTopComponent } from './components/scroll-to-top/scroll-to-top.c
     HeaderComponent,
     FooterComponent,
     ScrollToTopComponent,
+    AboutComponent,
+    LoginComponent,
+    ApiComponent,
   ],
   imports: [
     BrowserModule,
