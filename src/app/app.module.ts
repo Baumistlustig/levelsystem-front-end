540a0fa0ea9982b2405accea8116ccd2546b12ef
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-06-11 23:33:59 +0200

    Web-Deployment

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { RouterModule, Routes } from "@angular/router";
+import {RouterModule, Routes} from "@angular/router";
+
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
