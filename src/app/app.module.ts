3dedea712747848791e4f30c498583aa1b99db20
johannes <johannes.hoellwerth@protonmail.com>
2022-06-11 10:51:18 +0200

    Added Routes; Moved main content from app.component.html to root.component.html; Added data-policy; Added impress;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule, Routes } from "@angular/router";
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from "@angular/common/http";
@@ -10,6 +11,15 @@ import { AboutComponent } from './components/about/about.component';
 import { LoginComponent } from './components/login/login.component';
 import { ApiComponent } from './components/api/api.component';
 import { LeaderboardComponent } from './components/leaderboard/leaderboard.component';
+import { ImpressComponent } from './components/impress/impress.component';
+import { DataPolicyComponent } from './components/data-policy/data-policy.component';
+import { RootComponent } from './components/root/root.component';
+
+const myRoutes: Routes = [
+  { path: '', component: RootComponent},
+  { path: 'impress', component: ImpressComponent},
+  { path: 'data-policy', component: DataPolicyComponent},
+];
 
 @NgModule({
   declarations: [
@@ -21,8 +31,12 @@ import { LeaderboardComponent } from './components/leaderboard/leaderboard.compo
     LoginComponent,
     ApiComponent,
     LeaderboardComponent,
+    ImpressComponent,
+    DataPolicyComponent,
+    RootComponent,
   ],
   imports: [
+    RouterModule.forRoot(myRoutes),
     BrowserModule,
     HttpClientModule,
   ],
