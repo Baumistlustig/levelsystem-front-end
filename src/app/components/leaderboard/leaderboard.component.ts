e922294f37c1c4596f32189eb4a6ad53e2a4a0d6
Baumistlustig <johannes.hoellwerth@protonmail.com>
2022-06-21 20:46:23 +0200

    Updated Leaderboard

--- src/app/components/leaderboard/leaderboard.component.ts
+++ src/app/components/leaderboard/leaderboard.component.ts
@@ -13,9 +13,10 @@ export class LeaderboardComponent implements OnInit {
   constructor(private client: HttpClient) {  }
 
   ngOnInit(): void {
-    this.client.get('http://localhost:8090/api/leaderboard')
+    this.client.get('http://localhost:3000/api/leveling/leaderboard/5')
       .subscribe(data => {
         this.data = data;
+
         if (data === { "error": "Not enough users in database!" }) {
           throw data;
         }
